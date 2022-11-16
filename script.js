4595964a10d9b00cc3f489844657a115a2632095
RandalRainisKruus <randalrainiskruus@gmail.com>
2022-11-16 21:58:15 +0200

    Kontaktileht ja JS fail

---
+++ script.js
@@ -0,0 +1,3 @@
+// funktsioon https://youtu.be/MCRV9ajSfoc?t=321
+// kui funktsioon välja kutsutakse, siis kerib lehe sujuvalt üles.
+const toTop = () => window.scrollTo({top: 0, behavior: "smooth"});
